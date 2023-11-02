4c82df7211bdacda7a7eb49606f082612b72d279
Numchok Juntadung <Numchok.j@urcrice.com>
2023-11-02 11:46:13 +0700

    01

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\FullCalendarController;
  
  
-Route::get('fullcalendars', [FullCalendarController::class, 'index']);
+Route::get('/', [FullCalendarController::class, 'index']);
 Route::post('fullcalendar/create', [FullCalendarController::class, 'create']);
 Route::post('fullcalendar/update', [FullCalendarController::class, 'update']);
 Route::post('fullcalendar/delete', [FullCalendarController::class, 'destroy']);
