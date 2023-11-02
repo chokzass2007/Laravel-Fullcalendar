6dbc4a8184270a440f2d6bc6a77c3bbe6c6a3f07
Numchok Juntadung <Numchok.j@urcrice.com>
2023-11-02 09:19:59 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\FullCalendarController;
+ 
+ 
+Route::get('fullcalendars', [FullCalendarController::class, 'index']);
+Route::post('fullcalendar/create', [FullCalendarController::class, 'create']);
+Route::post('fullcalendar/update', [FullCalendarController::class, 'update']);
+Route::post('fullcalendar/delete', [FullCalendarController::class, 'destroy']);
+
